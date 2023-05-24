bc1988248587246688d3940b2abe990e17d8b904
mirza-z <zaimovic330@gmail.com>
2023-05-24 17:25:00 +0200

    sad

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TimerCountDownDisplay } from './TimerCountDownDisplay';
+import { TimerToggleButton } from './TimerToggleButton';
+import { TimerModeDisplay, TimerModes } from './TimerModeDisplay';
+
+const FOCUS_TIME_MINUTES = 25 * 60 * 1000;
+const BREAK_TIME_MINUTES = 10 * 60 * 1000;
 
 export default function App() {
+  const [ timerCount, setTimerCount ] = useState<number>(FOCUS_TIME_MINUTES);
+  const [ timerInterval, setTimerInterval ] = useState<NodeJS.Timer | null>(null);
+  const [ isTimerRunning, setIsTimerRunning] = useState<boolean>(false)
+  const [ timerMode, setTimerMode] = useState<TimerModes>("Focus")
+
+  useEffect(()=>{
+    if(timerCount === 0){
+      if(timerMode === 'Focus'){
+        setTimerMode('Break');
+        setTimerCount(BREAK_TIME_MINUTES);
+      } else{
+        setTimerMode('Focus');
+        setTimerCount(FOCUS_TIME_MINUTES);
+      }
+      stopTimer();
+    }
+  },[timerCount])
+
+  const startTimer = () =>{ 
+    setIsTimerRunning(true)
+    const id = setInterval(()=>setTimerCount(prev => prev - 1000), 1000);
+    setTimerInterval(id);
+  };
+
+  const stopTimer = () => {
+    if(timerInterval !== null){
+      clearInterval(timerInterval)
+    }
+    setIsTimerRunning(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{...styles.container, ...{backgroundColor: timerMode==='Break'? '#2a9d8f' : '#d95550'}}}>
+      <TimerModeDisplay timerMode={timerMode} />
       <StatusBar style="auto" />
+      <TimerToggleButton isTimerRunning={isTimerRunning} startTimer={startTimer} stopTimer={stopTimer} />
+      <TimerCountDownDisplay timerDate={new Date(timerCount)}/>
     </View>
   );
 }
@@ -13,7 +53,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d95550',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ TimerModeDisplay.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export type TimerModes =  "Focus" | 'Break';
+type Props = {
+    timerMode: TimerModes
+}
+
+export const TimerModeDisplay: React.FC<Props> = ({timerMode}) =>{
+    return (
+        <View style={styles.container}>
+            <Text style={styles.timerModeText}>{timerMode} Time 🍅</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: '100%'
+    },
+    timerModeText:{
+        fontSize: 40,
+        fontWeight: '800',
+        color: '#fff'
+    }
+})
